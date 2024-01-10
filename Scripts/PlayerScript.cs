9a845cf5a2ffc316b97169cf6daffb83dc26c844
ZeviCohen <123010327+ZeviCohen@users.noreply.github.com>
2024-01-09 21:14:27 -0500

    Added more movement code and jumping for 2d. Added collider to the player. Movement still has bugs.

--- Scripts/PlayerScript.cs
+++ Scripts/PlayerScript.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //movement variables
     private float horizontalInput;
     public float speed;
+    public bool canJump;
+    public float jumpHeight = 10.0f;
 
     private Rigidbody2D rb;
+    private bool using2d;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        using2d = true;
+
+        canJump = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        if (using2d)
+        {
+            //horizontal movement
+            horizontalInput = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+            //jump
+            if (canJump && Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+                canJump = false;
+            }
+        }
+    }
+
+    public void onCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("2dGround"))
+        {
+            canJump = true;
+        }
     }
 }
