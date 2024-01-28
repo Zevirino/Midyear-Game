84538cd8007e44c3dbdd44b5dc584e692ddc7605
Nikhil Palempalle <nikpal26@bergen.org>
2024-01-27 21:38:20 -0500

    Laser is now properly blocked by box

--- Scripts/LaserBlock.cs
+++ Scripts/LaserBlock.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class LaserBlock : MonoBehaviour
 {
-    Vector2 origPos; 
+    Vector2 origPos;
+    Vector2 origScale;
     // Start is called before the first frame update
     void Start()
     {
         origPos = new Vector2(transform.position.x, transform.position.y);
+        origScale = new Vector2(transform.localScale.x, transform.localScale.y);
     }
 
     // Update is called once per frame
@@ -18,18 +20,26 @@ public class LaserBlock : MonoBehaviour
 
     }
 
-    public void OnTriggerStay2D(Collider2D collider) {
+    public void OnTriggerEnter2D(Collider2D collider) {
         if (collider.gameObject.CompareTag("Box")) {
+            /*
             float y = origPos.y;
             float scale = GetComponent<BoxCollider2D>().size.x;
             float topEdge = collider.transform.position.y - collider.GetComponent<BoxCollider2D>().size.y/2 + 0.7f;
             transform.position = new Vector2(transform.position.x, y - (y + scale - topEdge));
+            */
+            float bottomEdge = transform.position.y - (transform.localScale.y * GetComponent<BoxCollider2D>().size.y) / 2.0f;
+            float topEdge = collider.transform.position.y - collider.GetComponent<BoxCollider2D>().size.y / 2 + 0.7f;
+            transform.position = new Vector3(transform.position.x, (bottomEdge + topEdge)/2.0f, transform.position.z);
+            float yScale = Math.Abs(topEdge - bottomEdge) / (GetComponent<BoxCollider2D>().size.y * transform.localScale.y);
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * yScale, transform.localScale.z);
         }
     }
 
     public void OnTriggerExit2D(Collider2D collider) {
         if (collider.gameObject.CompareTag("Box")) {
             transform.position = origPos;
+            transform.localScale = origScale;
         }
     }
 }
