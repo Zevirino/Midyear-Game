eb6500bf6d5b3a1e19a747ff48111977be46f92d
Nikhil Palempalle <nikpal26@bergen.org>
2024-01-30 21:52:43 -0500

    Almost done with Level 4
    Just need to work on breakable boxes

--- Scripts/For Camera/CameraFollow.cs
+++ Scripts/For Camera/CameraFollow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
+    public bool canFollowYAxis = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +18,21 @@ public class CameraFollow : MonoBehaviour
     {
         if (!GameManager.gameFreeze)
         {
+<<<<<<< Updated upstream
             transform.Translate(new Vector3((player.transform.position.x - transform.position.x)*Time.deltaTime, 0f,0f));
             //Camera only follows player vertically if they are above a certain point
             if (player.transform.position.y >= player.GetComponent<PlayerScript>().ogPos.y + 1f && player.GetComponent<PlayerScript>().canJump)
             {
                 transform.Translate(new Vector3(0f, (player.transform.position.y - transform.position.y) * Time.deltaTime, 0f));
+=======
+            if (canFollowYAxis)
+            {
+                transform.Translate(new Vector3((player.transform.position.x - transform.position.x) * Time.deltaTime, (player.transform.position.y - transform.position.y) * Time.deltaTime, 0));
+            }
+            else
+            {
+                transform.Translate(new Vector3((player.transform.position.x - transform.position.x) * Time.deltaTime, 0, 0));
+>>>>>>> Stashed changes
             }
         }
     }
